f30d9c035097866a4297442781d4bea1084ffe7e
ankurverma99 <vermaankur032@gmail.com>
2020-09-13 13:11:04 +0530

    1st prog to launch browser

---
+++ src/org/qsp/weekend/SeleniumDemo/DemoLaunch.java
@@ -0,0 +1,11 @@
+package org.qsp.weekend.SeleniumDemo;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemoLaunch {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", ".\\Software\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+	}
+
+}
